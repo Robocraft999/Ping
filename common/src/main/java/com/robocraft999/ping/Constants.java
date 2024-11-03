1cffbf8fd423ad00be568639f681e7ea40ada551
Robocraft999 <59525378+Robocraft999@users.noreply.github.com>
2024-11-03 15:09:48 +0100

    init

---
+++ common/src/main/java/com/robocraft999/ping/Constants.java
@@ -0,0 +1,11 @@
+package com.robocraft999.ping;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+	public static final String MOD_ID = "ping";
+	public static final String MOD_NAME = "Ping";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
---
+++ forge/src/main/java/com/robocraft999/ping/Ping.java
@@ -0,0 +1,18 @@
+package com.robocraft999.ping;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Constants.MOD_ID)
+public class Ping {
+
+    public Ping() {
+        // This method is invoked by the Forge mod loader when it is ready
+        // to load your mod. You can access Forge and Common code in this
+        // project.
+
+        // Use Forge to bootstrap the Common mod.
+        Constants.LOG.info("Hello Forge world!");
+        CommonClass.init();
+
+    }
+}
